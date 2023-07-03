d382bb212b5766e5cc4cd81e68a95775874b8b4e
p298vytp0waer9hgq0n <98211887+p298vytp0waer9hgq0n@users.noreply.github.com>
2023-07-03 20:47:37 +0500

    really add it back this time

---
+++ backend/ecosystem.config.js
@@ -0,0 +1,24 @@
+require('dotenv').config({ path: './.env.deploy' });
+
+const {
+  DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REF = 'origin/main',
+} = process.env;
+
+module.exports = {
+  apps: [{
+    name: 'app',
+    script: './dist/app.js',
+  }],
+
+  deploy: {
+    production: {
+      user: DEPLOY_USER,
+      host: DEPLOY_HOST,
+      ref: DEPLOY_REF,
+      repo: 'git@github.com:p298vytp0waer9hgq0n/web-plus-pm2-deploy',
+      path: DEPLOY_PATH,
+      'pre-deploy-local': `scp .env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}/backend`,
+      'post-deploy': 'cd backend && npm i && npm run build',
+    },
+  },
+};
